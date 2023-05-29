96c1954944d97f936db4d4c4c7eae576f5774da4
Kregor <Kregorlatt1221@gmail.com>
2023-05-29 17:30:10 +0300

    Edit detailspage

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,13 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+
+HttpClient httpClient;
+public MonkeyService()
+{
+    this.httpClient = new HttpClient();
+}
 }
